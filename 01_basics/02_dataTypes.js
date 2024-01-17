bf821f047e5e56e0b5b209e24f6c807b8e5b3565
Aditya Srivastava <adityasrivastaava@gmail.com>
2024-01-17 11:38:28 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Aditya")
+
+let name = "Aditya"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => it is a standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
